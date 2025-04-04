e4ad438b5f0c061fdaa4cce4cb128c74487ef160
Jurate Vilima <jurate.vilima@gmail.com>
2025-04-04 05:43:16 +0300

    Enable error reporting in index.php

--- backend/public/index.php
+++ backend/public/index.php
@@ -5,7 +5,8 @@ use App\Controller\Main;
 use FastRoute\Dispatcher;
 use FastRoute\RouteCollector;
 
-ini_set('display_errors', 0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once __DIR__ . '/../vendor/autoload.php';
